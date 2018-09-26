2534d944b966ebe0ae5a33417ef109dccedfd04b
Lauren Little <littlelauren016@gmail.com>
2018-09-25 21:51:06 -0400

    added teacher, class and assignment models

---
+++ models/class-model.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Require additional Models
+const TeacherModel = require('./teacher-model');
+
+const Schema = mongoose.Schema;
+
+const classSchema = new Schema({
+
+});
---
+++ models/teacher-model.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose');
+
+// Require additional Models
+const ClassModel = require('./class-model');
+
+const Schema = mongoose.Schema;
+
+const teacherSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  encryptedPassword: {
+    type: String,
+    required: true
+  },
+  studentKeyCode: {
+    type: String,
+    required: true
+  },
+  subjectsTaught: {
+    type: Array,
+    default: [],
+    required: true
+  },
+  class: ClassModel.schema
+},
+{
+  timestamps: true
+});
+
+
+const TeacherModel = mongoose.model('Teacher', teacherSchema);
+// Collection name
+// User -> users -> db.users.find()
+
+module.exports = TeacherModel;
+// Connects the  model above to the routes file
